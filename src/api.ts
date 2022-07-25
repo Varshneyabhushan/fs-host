02457347b7cf5d47cecf88e693346331871bdcf1
varshneyabhushan <me@bhushan.fun>
2022-07-25 15:30:12 +0530

    pass logger to api

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,14 @@
 import { RequestHandler } from "express";
+import { Logger } from "winston";
 
 export interface DownloadConfig {
    downloadPath: string;
    requestTimeout: number;
 }
 
-export default function makeFileUploadEndPoint(config: DownloadConfig): RequestHandler {
+export default function makeFileUploadEndPoint(
+   config: DownloadConfig,
+   logger: Logger,
+): RequestHandler {
    return (req, res) => {};
 }
