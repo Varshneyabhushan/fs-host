828d5d3e66c55ce8761b04f4c0404c884b6ac90a
varshneyabhushan <me@bhushan.fun>
2022-07-25 15:29:07 +0530

    added conversions util

---
+++ src/utils/conversions.ts
@@ -0,0 +1,19 @@
+import { AddressInfo } from "node:net";
+
+export function toNumber(numberString: string): number | null {
+   let anticipation = +numberString;
+   if (Number.isNaN(anticipation)) return null;
+   return anticipation;
+}
+
+export function getAddressInfo(address: string | AddressInfo | null): string {
+   if (!address) return "";
+   if (typeof address === "string") return address;
+   let { family, address: addressString, port } = address;
+   return `${family}:${addressString}:${port}`;
+}
+
+export function toErrorObject(e: Error) {
+   let { name, message, stack } = e;
+   return { name, message, stack };
+}
